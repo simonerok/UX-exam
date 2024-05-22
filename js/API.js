1d0ada8abf1627d8781e2a83ffad4eb4075729a8
Simone <ssimone12@gmail.com>
2024-05-22 12:16:16 +0200

    frontpage working

--- js/API.js
+++ js/API.js
@@ -1,58 +1,6 @@
 'use strict';
- const API_KEY = "1";
 
-
-// Search meal by name
-const SEARCH_MEAL_BY_NAME_URL = "search.php?s=Arrabiata";
-
-// List all meals by first letter
-const LIST_ALL_MEALS_BY_FIRST_LETTER = "search.php?f=a";
-
-// Lookup full meal details by id
-const LOOKUP_FULL_MEAIL_DETAILS_BY_ID = "lookup.php?i=52772";
-
-// Lookup a single random meal
-const LOOKUP_SINGLE_RANDOM_MEAIL = "random.php";
-
-// List all meal categories
-const LIST_ALL_MEAL_CATEGORIES = "categories.php";
-
-// List all Categories, Area, Ingredients
-const LIST_ALL_CATEGORIES_AREA_INGREDIENTS = "list.php?c=list";
-
-// Filter by main ingredient
-const FILTER_BY_MAIN_INGREDIENT = "filter.php?i=chicken_breast";
-
-// Filter by Category
-const FILTER_BY_CATEGORY = "filter.php?c=Seafood";
-
-// Filter by Area
-const FILTER_BY_AREA= "filter.php?a=Canadian";
-
-
-
-
-
-async function testApi(params) {
-    try {
-      const response = await fetch("https://www.themealdb.com/api/json/v1/1/" + params);
-      if (response.ok) {
-        const data = await response.json();
-
-        console.log(data.meals);
-        console.log('API Response:', data);
-      } else {
-        console.error('API request failed with status:', response.status);
-      }
-    } catch (error) {
-      console.error('API request failed with error:', error);
-    }
-  }
-
-  testApi(FILTER_BY_AREA);
-
-
-/****************************************  FRONTPAGE ************************************************/
+/****************************************  FETCH DATA FROM API TO BROWSE BY CATEGORY CARDS ************************************************/
 
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 
@@ -84,10 +32,9 @@ async function fetchMealFrontpage() {
 // Create an array of promises in order to laod 6 meals
 const promises = Array.from({length: 6}, () => fetchMealFrontpage());
 
-// Use Promise.all to fetch all the meals concurrently
+// Use Promise.all to fetch all the meals using more API calls
 Promise.all(promises)
     .then(meals => {
-      console.log("test2"); 
         // Get the template and the meal container
         const mealContainer = document.querySelector('#meal_container');
         const templateCard = document.querySelector('#template_card');
@@ -101,6 +48,7 @@ Promise.all(promises)
             const cardCategory = cardClone.querySelector('.card_category');
             const cardArea = cardClone.querySelector('.card_area');
             const youtubeLink = cardClone.querySelector('.card_youtube');
+            const categoryIcon = cardClone.querySelector('.card_category_icon span'); 
 
             cardImg.src = meal.strMealThumb;
             cardTitle.textContent = meal.strMeal;
@@ -108,6 +56,7 @@ Promise.all(promises)
             cardArea.textContent = "Cuisine: " + meal.strArea;
             youtubeLink.href = meal.strYoutube;
             youtubeLink.textContent = 'Watch on YouTube';
+            categoryIcon.textContent = meal.strCategory.charAt(0); // First letter of the category
 
             mealContainer.appendChild(cardClone);
         });
@@ -117,78 +66,164 @@ Promise.all(promises)
 
 
 
-// SEARCH BAR FUNCTIONALITY (fetching a meal by name lamb etc)
-const searchButton = document.querySelector('.search_box');
-const searchInput = document.querySelector('input');
-
-searchButton.addEventListener('click', async function() {
-  const searchTerm = searchInput.value;
-  let meals = await fetchData(`search.php?s=${searchTerm}`);
-
-  // If no meals were found by name, try searching by ingredient
-  if (!meals) {
-    meals = await fetchData(`filter.php?i=${searchTerm}`);
-  }
-
-  // Select the error container and clear it
-  const errorContainer = document.querySelector('.error_container');
-  errorContainer.innerHTML = '';
-
-  // If no meals were found, display an error message
-  if (!meals) {
-    const errorMessage = document.createElement('p');
-    errorMessage.textContent = 'Sorry, no meals found. Try to search by an ingredient or the title of the meal you wish to see!';
-    errorMessage.style.color = 'red';
-    errorContainer.appendChild(errorMessage);
-    return;
-  }
-
-  // Extract the data into the UI card
-  const meal = meals[0];
-  const mealName = meal.strMeal;
-  const mealDescription = meal.strInstructions;
-  const mealImage = meal.strMealThumb;
-
-  // Select the elements
-  const cardImg = document.querySelector('.card_img');
-  const cardTitle = document.querySelector('.card_title');
-  const cardDescription = document.querySelector('.card_description');
-
-  // Replace the hardcoded values
-  cardImg.src = mealImage;
-  cardTitle.textContent = mealName;
-  cardDescription.textContent = mealDescription;
-});
-
-
-/*  fetch data for filterbutton */
+/****************************************  FILTER FUNCTIONALITY (filter by category) ************************************************/
 
 const select = document.querySelector('#filter_btn');
 const template = document.querySelector('#template_card');
 const container = document.querySelector('#meal_container');
 
-/* fetch data based on the selected "change" in the filter button */
+/* fetch data based on the selected "category" in the filter button */
 select.addEventListener('change', async function() {
     const category = select.value;
 
-    // Fetch data from the API
+    // Fetch data from the API by category
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
     const data = await response.json();
 
-    // Clear the container
+    // Clear the html to be able to place new data
     container.innerHTML = '';
 
     // Create a new card for each meal
-    data.meals.forEach(meal => {
+    const mealPromises = data.meals.map(async meal => {
+        // Fetch additional data for the meal
+        const mealResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`);
+        const mealData = await mealResponse.json();
+        const detailedMeal = mealData.meals[0];
+
         // Clone the template
         const clone = template.content.cloneNode(true);
 
         // Populate the clone with data
-        clone.querySelector('.card_img').src = meal.strMealThumb;
-        clone.querySelector('.card_title').textContent = meal.strMeal;
+        clone.querySelector('.card_img').src = detailedMeal.strMealThumb;
+        clone.querySelector('.card_title').textContent = detailedMeal.strMeal;
         clone.querySelector('.card_category').textContent = category;
+        clone.querySelector('.card_category_icon span').textContent = category.charAt(0);
+
+        // Select cardArea and youtubeLink within the clone
+        const cardArea = clone.querySelector('.card_area');
+        const youtubeLink = clone.querySelector('.card_youtube');
+
+        if (cardArea) {
+            cardArea.textContent = "Cuisine: " + detailedMeal.strArea;
+        }
+
+        if (youtubeLink) {
+            if (detailedMeal.strYoutube) {
+                youtubeLink.href = detailedMeal.strYoutube;
+                youtubeLink.textContent = 'Watch on YouTube';
+            } else {
+                youtubeLink.textContent = 'No YouTube guide available';
+            }
+        }
 
         // Append the clone to the container
         container.appendChild(clone);
     });
+
+    // Wait for all meals to be fetched and cards to be created to get all the information
+    await Promise.all(mealPromises);
 });
+
+ /****************************************  SEARCH BAR FUNCTIONALITY (fetching a meal by name lamb etc) ************************************************/
+// Select the search input and button from the DOM
+const searchInput = document.querySelector('#search_input');
+const searchButton = document.querySelector('#search_button');
+
+// Select the section where the meals will be displayed
+const section = document.querySelector('#meal_of_the_day_container');
+
+// Select the card template
+const cardContainer = document.querySelector('#section_1_card');
+
+// Select the h2 element
+const heading = document.querySelector('#meal_of_th_day');
+
+// Add an event listener to the search button
+searchButton.addEventListener('click', async function(e) {
+  // Prevent the form from submitting
+  e.preventDefault();
+
+  // Get the value of the search input
+  const searchTerm = searchInput.value;
+
+  // Fetch the meals from the API
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`);
+  const data = await response.json();
+
+  // Check if any meals were found
+  if (data.meals === null) {
+    section.textContent = 'No meals found. try to seach for a name of the meal or a meal ingredient.';
+    return;
+  }
+
+  // Clear the previous search results
+  section.innerHTML = '';
+
+  // Change the text of the h2 element
+  heading.textContent = 'Meals you can try:';
+
+  // Create a card for each meal
+  data.meals.slice(0, 6).forEach(meal => {
+    // Clone the card template
+    const card = cardContainer.content.cloneNode(true);
+
+    // Get the elements in the card
+    const img = card.querySelector('.card_img');
+    const title = card.querySelector('.card_title');
+    const description = card.querySelector('.card_description');
+    const categoryIcon = card.querySelector('.card_category_icon span');
+    const category = card.querySelector('.card_category');
+
+    // Set the properties of the elements
+    img.src = meal.strMealThumb;
+    img.alt = meal.strMeal;
+    title.textContent = meal.strMeal;
+    description.textContent = meal.strInstructions;
+    categoryIcon.textContent = meal.strCategory.charAt(0); // First letter of the category
+    category.textContent = meal.strCategory;
+
+    // Append the card to the section
+    section.appendChild(card);
+  });
+});
+
+/****************************************  FETCH RANDOM MEAL FOR MEAL OF THE DAY CARD ************************************************/
+/* select the html element and ensure taht the con tainer can be cloned multiple times  */
+const cardTemplate = document.querySelector('#section_1_card').content.cloneNode(true);
+
+async function mealOfTheDay() {
+  // Fetch a random meal
+  const meal = await fetchRandomMeal();
+  console.log(meal); // Log the meal to the console
+
+  // Clone the card template
+  const card = cardTemplate.cloneNode(true);
+
+  // Get the elements in the card
+  const img = card.querySelector('.card_img');
+  const title = card.querySelector('.card_title');
+  const description = card.querySelector('.card_description');
+  const favIcon = card.querySelector('.btn_fav img');
+  const categoryIcon = card.querySelector('.card_category_icon');
+  const category = card.querySelector('.card_category');
+
+  // Set the properties of the elements
+  img.src = meal.strMealThumb;
+  img.alt = meal.strMeal;
+  title.textContent = meal.strMeal;
+  description.textContent = meal.strInstructions;
+  favIcon.src = '../heart-no-fill.svg';
+  favIcon.alt = 'favorite Icon';
+  categoryIcon.textContent = meal.strCategory.charAt(0); // First letter of the category
+  category.textContent = meal.strCategory;
+
+  // Append the card to the meal of the day container
+  const mealOfTheDayContainer = document.querySelector('#meal_of_the_day_container');
+  console.log(mealOfTheDayContainer); // Log the container to the console
+  mealOfTheDayContainer.appendChild(card);
+}
+
+// Call the function
+mealOfTheDay();
+
+ 
