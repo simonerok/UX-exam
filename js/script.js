5a53b56ecd66accc21112b2e8e87da374ff968e3
Mathilde <math68n8@stud.kea.dk>
2024-05-28 11:21:49 +0200

    header done

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const navToggle = document.getElementById('nav-toggle');
     const mainMenu = document.getElementById('main-menu');
 
+    console.log(navToggle);
+    console.log(mainMenu);
+    
     navToggle.addEventListener('click', function() {
         mainMenu.classList.toggle('shown');
         if (mainMenu.classList.contains('shown')) {
